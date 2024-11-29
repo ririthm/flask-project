a30908f7350e3e4836d35b2989f3d466bac8417c
Riri <fatihkarimy@gmail.com>
2024-11-29 20:10:28 +0700

    Simpan perubahan sebelum merge

--- board/posts.py
+++ board/posts.py
@@ -5,7 +5,8 @@ from flask import (
     request,
     url_for,
 )
-from board.database import get_db, get_pg_db_conn
+
+from board.database import get_db
 
 bp = Blueprint("posts", __name__)
 
@@ -14,23 +15,23 @@ def create():
     if request.method == "POST":
         author = request.form["author"] or "Anonymous"
         message = request.form["message"]
+
         if message:
-            db = get_pg_db_conn()
-            cur = db.cursor()
-            cur.execute("INSERT INTO post (author, message) VALUES (%s, %s)", (author, message))
+            db = get_db()
+            db.execute(
+                "INSERT INTO post (author, message) VALUES (?, ?)",
+                (author, message),
+            )
             db.commit()
-            cur.close()
-            db.close()
-            return redirect(url_for("posts.post"))
-    
+            return redirect(url_for("posts.posts"))
+
     return render_template("posts/create.html")
 
 @bp.route("/posts")
-def post():
-    db = get_pg_db_conn()
-    cur = db.cursor()
-    cur.execute("SELECT author, message, created FROM post ORDER BY created DESC")
-    posts = cur.fetchall()
-    cur.close()
-    db.close()
+def posts():
+    db = get_db()
+    posts = db.execute(
+            "SELECT author, message, created FROM post ORDER BY created DESC"
+            ).fetchall()
     return render_template("posts/posts.html", posts=posts)
+            
